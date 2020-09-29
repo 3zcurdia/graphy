0298cd093a2afad1f2d33cebb6060930e85ea77e
Luis Ezcurdia Razo <ing.ezcurdia@gmail.com>
2020-09-28 19:00:13 -0500

    Add graph wrapper and remove nodes from registry

--- lib/graphy/registry.rb
+++ lib/graphy/registry.rb
@@ -3,19 +3,26 @@ require 'singleton'
 module Graphy
   class Registry
     include Singleton
-    attr_accessor :nodes, :edges
+    attr_accessor :edges
 
-    def initialize
-      @nodes = {}
-      @edges = []
+    def self.edge?(source, destiny)
+      instance.edge?(source, destiny)
     end
 
-    def node?(name)
-      nodes.key?(name)
+    def self.add_edge(source, destiny)
+      instance.add_edge(source, destiny)
+    end
+
+    def initialize
+      @edges = {}
     end
 
     def edge?(source, destiny)
-      edges.any? { |x| [source, destiny] == x }
+      edges.key?["#{source}->#{destiny}"]
+    end
+
+    def add_edge(source, destiny, options = {})
+      self.edges["#{source}->#{destiny}"] = options
     end
   end
 end
